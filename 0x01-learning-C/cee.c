0080200dfdb49709d4c42e2aa4dabdfdc96f7214
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-16 18:27:38 +0300

    Update cee.c

--- 0x01-learning-C/cee.c
+++ 0x01-learning-C/cee.c
@@ -9,42 +9,31 @@
 
 int main()
 {
-  char card_name[3];
-  puts("enter the card_name: ");
-  scanf("%2s", card_name);
-  int val = 0;
-  if (card_name[0] == 'K') {
-    
-    val = 10;
-    
-  } else if (card_name[0] == 'Q') {
-    val = 10;
-  }else if (card_name[0] == 'J') {
-    
-    val = 10;
-    
-  } else if (card_name[0] == 'A') {
-    val = 11;
-  }else {
-    
-    val = atoi(card_name);
-    
-  }
-  
-  printf("The card value is: %i\n", val);
-
-  return 0;
+char card_name[3];
+puts("enter the card_name: ");
+scanf("%2s", card_name);
+int val = 0;
+if (card_name[0] == 'K')
+{
+val = 10;
 }
-    
-  
-
-
-
-
-
-
-
-
-
+else if (card_name[0] == 'Q')
+{
+val = 10;
+}
+else if (card_name[0] == 'J')
+{
+val = 10;
+}
+else if (card_name[0] == 'A')
+{
+val = 11;
+}
+else
+{
+val = atoi(card_name);
+}
+printf("The card value is: %i\n", val);
+return 0;
 }
 
