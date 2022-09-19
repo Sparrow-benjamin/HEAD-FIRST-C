202a06b0388439f1ded2c317516959843b2e0e77
Sparrow-benjamin <111073802+Sparrow-benjamin@users.noreply.github.com>
2022-09-19 07:44:21 +0300

    Update southeast.c

--- 0x01-learning-C/southeast.c
+++ 0x01-learning-C/southeast.c
@@ -7,17 +7,16 @@
 
 void go_south_east(int lat, int lon)
 {
-  lat = lat - 1;
-  lon = lon + 1;
+ lat = lat - 1;
+ lon = lon + 1;
 }
-
-int main()
+int main(void)
 {
-  int latitude = 32;
-  int longitude = -64
-    go_south_east(latitude, longitude);
-  printf("Avast! Now at: [%i, %i]\n", latitude, longitude);
-  return 0;
+int latitude = 32;
+int longitude = -64
+go_south_east(latitude, longitude);
+printf("Avast! Now at: [%i, %i]\n", latitude, longitude);
+return 0;
 }
     
   
