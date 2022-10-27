f4e013bba96fb47d0e7deffb5b1942af2e1bf3f1
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-10-28 01:45:49 +0300

    Update malloc.c

--- 0x01-learning-C/malloc.c
+++ 0x01-learning-C/malloc.c
@@ -4,18 +4,17 @@
 void k(int m0, int m1, int m2)
 
 {
-  int *l;
-  int sum;
-    
-  l=malloc(sizeof(*l) *3);
-  l[0]=m0;
-  l[1]=m1;
-  l[2]=m2;
-  sum=l[0]+l[1]+l[2];
-  printf("%d+%d+%d=%d\n", l[0],l[1],l[2],sum);
+int *l;
+int sum;
+l=malloc(sizeof(*l) *3);
+l[0]=m0;
+l[1]=m1;
+l[2]=m2;
+sum=l[0]+l[1]+l[2];
+printf("%d+%d+%d=%d\n", l[0],l[1],l[2],sum);
 }
-
-int main() {
-  k(1000,2000,3000);
-  return 0;
+int main()
+{
+k(1000,2000,3000);
+return 0;
 }
