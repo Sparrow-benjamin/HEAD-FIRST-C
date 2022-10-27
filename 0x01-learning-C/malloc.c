03c97a7d9a74c122b75e6f9034bcdbcc1ec9de13
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-10-28 01:48:46 +0300

    Update malloc.c

--- 0x01-learning-C/malloc.c
+++ 0x01-learning-C/malloc.c
@@ -1,15 +1,13 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 
 void k(int m0, int m1, int m2)
-
 {
 int *l;
 int sum;
 l=malloc(sizeof(*l) *3);
-l[0]=m0;
-l[1]=m1;
-l[2]=m2;
+l[0] = m0;
+l[1] = m1;
+l[2] = m2;
 sum=l[0]+l[1]+l[2];
 printf("%d+%d+%d=%d\n", l[0],l[1],l[2],sum);
 }
