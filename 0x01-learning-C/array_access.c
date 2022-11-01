057263a4076b868c485474e5a9fe3ff5ed15be90
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2022-11-01 16:17:40 +0300

    Update array_access.c

--- 0x01-learning-C/array_access.c
+++ 0x01-learning-C/array_access.c
@@ -5,14 +5,14 @@
 */
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 int i[5];
-i[0]=100;
-i[1]=200;
-i[2]=300;
-i[3]=400;
-i[4]=500;
+i[0] = 100;
+i[1] = 200;
+i[2] = 300;
+i[3] = 400;
+i[4] = 500;
 printf("Value of element i[0]=%d\n", i[0]);
 printf("Value of element i[1]=%d\n", i[1]);
 printf("Value of element i[2]=%d\n", i[2]);
@@ -23,5 +23,5 @@ printf("The address of element i[1]=%p\n", i[1]);
 printf("The address of element i[2]=%p\n", i[2]);
 printf("The address of element i[3]=%p\n", i[3]);
 printf("The address of element i[4]=%p\n", i[4]);
-return 0;
+return (0);
 }
